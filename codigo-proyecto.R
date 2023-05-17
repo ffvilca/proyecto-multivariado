920db3fae6f34127d979549e2b4279f7ab078079
Vilca <fvilca@uc.cl>
2023-05-17 15:16:46 -0600

    crear archivos

---
+++ codigo-proyecto.R
@@ -0,0 +1,20 @@
+# Proyecto -----
+# 
+# Analisis Multivariado 
+# 
+# Mauricio Goméz
+# Francisca Vilca Sanchez
+
+# PAQUETES A USAR ----
+
+library(rio)
+library(dplyr)
+library(beepr)
+library(ggplot2)
+library(ggrepel)
+library(kableExtra)
+library(TeachingDemos)
+library(pracma)
+library(mvnormtest)
+library(MVN)
+library(EnvStats)
