4ceefee4cab332a7a953bcb2bf0d9a6db1f7df97
Mau Gomez <maugomez@ciencias.unam.mx>
2023-05-22 14:05:55 -0600

    VA

--- codigo-proyecto.R
+++ codigo-proyecto.R
@@ -18,8 +18,28 @@ library(pracma)
 library(mvnormtest)
 library(MVN)
 library(EnvStats)
+library(FactoMineR)
+library(factoextra)
+library(tidyverse)
 
 datos <- rio::import("salud_fetos.csv")
 
 str(datos)
 View(datos)
+
+x11()
+pairs(datos)
+
+PCA_centrado=prcomp(datos, center=TRUE, scale=TRUE)
+PCA
+PCA_centrado
+fviz_screeplot(PCA, main="PCA",  addlabels = TRUE)
+fviz_screeplot(PCA_centrado, main="PCA centrado y reescalado",  addlabels = TRUE)
+summary(PCA_centrado)
+
+corrplot::datos
+fviz_pca_var(PCA_centrado,col.var="contrib",
+             gradient.cols = c("yellow", "red"),
+             repel = TRUE,  
+)
+rename(datos, )
